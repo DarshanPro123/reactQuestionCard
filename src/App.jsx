89badcd66a9808a8f99a76c790b3b78d327b1d45
Darshanpro123 <darshanpro187@gmail.com>
2024-09-23 19:25:32 +0530

    Add all updated files

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import questions from "../public/data.js";
+import { useState } from "react";
+
+function App() {
+  const [selectId, setSelectId] = useState(null);
+  const handleClick = (id) => {
+    setSelectId(id !== selectId ? id : null);
+  };
+  return (
+    <>
+      <h1>my app</h1>
+
+      <div className="cards">
+        {questions.map((qus) => {
+          return (
+            <div
+              onClick={() => handleClick(qus.id)}
+              className={`card ${qus.id === selectId ? "card-selected" : ""}`}
+              key={qus.id}
+            >
+              <h4>{qus.id === selectId ? qus.answer : qus.question}</h4>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
